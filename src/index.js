5571e0a9cf9ae417edf405ccd2691e416e0b3e3d
swei1 <swei1@amarsoft.comcccccc>
2017-08-11 08:59:30 +0800

    init webpack demo

---
+++ src/index.js
@@ -0,0 +1,24 @@
+/**
+ * Created by vison on 17/8/10.
+ */
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png';
+
+function component() {
+    var element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    element.classList.add('hello');
+
+    // Add the image to our existing div.
+    var myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    return element;
+}
+
+document.body.appendChild(component());
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+/**
+ * Created by vison on 17/8/10.
+ */
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            }
+        ]
+    }
+};
